d45b68d930b0bf49ab0de297b2ec980cb68cf141
aurda012 <alfredo.i.urdaneta@gmail.com>
2017-07-07 04:13:32 -0400

    Initial Commit

---
+++ Vape Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vape Collector
+//
+//  Created by Alfredo Urdaneta on 7/7/17.
+//  Copyright © 2017 AlfredosApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
